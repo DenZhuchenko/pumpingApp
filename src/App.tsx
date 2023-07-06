783268954034557855cb9d53fe03e20c9887c64f
DenZhuchenko <NavisNobilitae@gmail.com>
2023-07-06 21:41:44 +0300

    React router dom + react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./components/pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./components/pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className={'app'}>
-            xDDDDDDDDDDDDDDDDD
-            <Counter/>
+            <Link to={'/'}>Main Page</Link>
+            <Link to={'/about'}>About Page</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
